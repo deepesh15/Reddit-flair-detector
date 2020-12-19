63448558b309b523a394782d7604e0402dc0854d
Deepesh Yadav <39853188+deepesh15@users.noreply.github.com>
2020-12-19 11:31:37 +0530

    Delete req.py

--- website/req.py
+++
@@ -1,6 +0,0 @@
-import requests
-url ='https://flair-classifier.herokuapp.com/automated_testing'
-
-files = {'upload_file': open('file.txt','r')}
-r = requests.post(url, files=files)
-print(r.content)
